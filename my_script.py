53c31a9d93e89dd9ca9accd83f493647713b9b45
Sheil-Patel <59850027+Sheil-Patel@users.noreply.github.com>
2020-03-23 17:39:53 -0400

    Done stuff

--- my_script.py
+++ my_script.py
@@ -1,3 +1,17 @@
 
 def enlarge(i):
     return i * 100
+
+#need to remove from global scope in order to import other things from this script
+#my_number = float(input("Please input a number:"))
+##n = enlarge(8)
+#n = enlarge(my_number)
+#print("ENLARGING NUMBER..." + str(n))
+
+if __name__ == "__main__":
+    # only run this if invoked from command-line
+    # not if imported by another file
+    my_number = float(input("Please input a number:"))
+    #n = enlarge(8)
+    n = enlarge(my_number)
+    print("ENLARGING NUMBER..." + str(n))
