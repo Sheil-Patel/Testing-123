9e734d791269bf930c03c522ec40ee0fd92bc70e
Sheil-Patel <59850027+Sheil-Patel@users.noreply.github.com>
2020-03-23 18:08:14 -0400

    Last commit

--- Test/my_test.py
+++ Test/my_test.py
@@ -3,3 +3,10 @@ from App.my_script import enlarge
 def test_enlarge():
     result = enlarge(3)
     assert result == 300
+
+
+    #assert determine_winner("rock","rock") == None
+    #assert determine_winner("rock" , "paper") == "paper"
+    # ... etc. , etc. , ...
+
+    
